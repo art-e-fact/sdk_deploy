74d3cbf4f7819d6dea80f075bf4c8792a7356e97
Bach Nguyen <ngb1998@gmail.com>
2026-04-23 13:04:30 +0900

    add rtabmap modes + launch file (manual mapping) + configs + refactor folders

---
+++ src/Lite3_sdk_deploy/interface/robot/launch/mujoco_simulation_ros2.launch.py
@@ -0,0 +1,122 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+
+def launch_setup(context, *args, **kwargs):
+    mode = int(LaunchConfiguration('mode').perform(context))
+    localization = LaunchConfiguration('localization').perform(context)
+    localization = localization == 'True' or localization == 'true'
+    nav2_params_filepath_launch_arg = LaunchConfiguration('nav2_params_filepath')
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    database_path = LaunchConfiguration("database_path")
+
+    nav2_package_share = FindPackageShare("nav2_bringup").perform(context)
+    lite3_package_share = FindPackageShare("lite3_sdk_deploy").perform(context)
+
+    nav2_launch = (
+        f"{nav2_package_share}"
+        "/launch/navigation_launch.py"
+    )
+    rviz_launch = (
+        f"{nav2_package_share}"
+        "/launch/rviz_launch.py"
+    )
+
+    if mode == 0:
+        rtabmap_mode = "lidar"
+        rviz_filepath = f"{lite3_package_share}/config/mapping_lidar.rviz"
+    elif mode == 1:
+        rtabmap_mode = "rgbd"
+        rviz_filepath = f"{lite3_package_share}/config/mapping_rgbd.rviz"
+    else:
+        rtabmap_mode = "rgbd_lidar"
+        rviz_filepath = f"{lite3_package_share}/config/mapping_rgbd_lidar.rviz"
+
+    rtabmap_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(f"{lite3_package_share}/launch/rtabmap_{rtabmap_mode}.launch.py"),
+        launch_arguments={
+            "use_sim_time": use_sim_time,
+            "localization": "false",
+            "database_path": database_path,
+        }.items(),
+    )
+
+    return [
+
+        # MuJoCo simulation
+        Node(
+            package='lite3_sdk_deploy', 
+            executable='mujoco_simulation_ros2.py',
+            output='screen',
+            parameters = [{
+                'use_procedural_scene': True,
+                'procedural_env_seed': 1234,
+            }],
+        ),
+
+        # RL controller with twist input
+        Node(
+            package='lite3_sdk_deploy', 
+            executable='rl_deploy',
+            output='screen',
+            arguments=['--twist']
+        ),
+
+        # RTAB-Map
+        rtabmap_launch, 
+
+        # Navigation (planner + controller)
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch]),
+            launch_arguments=[
+                ('params_file', nav2_params_filepath_launch_arg)
+            ]
+        ),   
+
+        # RViz2
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([rviz_launch]),
+            launch_arguments=[
+                ('rviz_config', rviz_filepath)
+            ]
+        )
+    ]
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument("use_sim_time", default_value="false"),
+        DeclareLaunchArgument("database_path", default_value="~/.ros/rtabmap.db"),
+        
+        DeclareLaunchArgument(
+            'mode', default_value='2',
+            description='RTAB-Map mode: 0 (lidar), 1 (rgbd), 2 (lidar+rgbd). Default: 2'),
+
+        DeclareLaunchArgument(
+            'localization', default_value='true',
+            description='Launch in localization mode.'),
+
+        DeclareLaunchArgument(
+            'nav2_params_filepath',
+            default_value=PathJoinSubstitution([
+                get_package_share_directory('lite3_sdk_deploy'),
+                'config', 'nav2_params.yaml'
+            ]),
+            description='the file path to Nav2 params'
+        ),
+
+        DeclareLaunchArgument(
+            'nav2_rviz_filepath',
+            default_value=PathJoinSubstitution([
+                get_package_share_directory('lite3_sdk_deploy'),
+                'config', 'mapping2.rviz'
+            ]),
+            description='the file path to Nav2 rviz config'
+        ),
+
+        OpaqueFunction(function=launch_setup)
+    ])
---
+++ src/Lite3_sdk_deploy/interface/robot/launch/rtabmap_rgbd_lidar.launch.py
@@ -0,0 +1,114 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    headless = LaunchConfiguration("headless")
+    use_rtabmap_rviz = LaunchConfiguration("use_rtabmap_rviz")
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    database_path = LaunchConfiguration('database_path')
+    localization = LaunchConfiguration('localization')
+    localization = localization == 'True' or localization == 'true'
+
+    arguments = []
+
+    parameters = {
+        'use_sim_time': use_sim_time,
+        'subscribe_scan': True,
+        'subscribe_depth': True,
+        'subscribe_rgb': True,
+        'subscribe_odom_info': False,
+        'frame_id': 'base_link',
+        'odom_frame_id': 'odom',
+        'map_frame_id': 'map',
+        'publish_tf': True,
+        'use_action_for_goal': True,
+        'database_path': database_path,
+        'queue_size': 30,
+        'approx_sync': True,
+
+        ### Grid map
+        'Grid/Sensor': '2', # Create occupancy grid from selected sensor: 0=laser scan, 1=depth image(s) or 2=both laser scan and depth image(s)
+        'Grid/RangeMin': '0.3',  # ignore laser hits on the robot body
+        'Grid/RangeMax': '12.0',  # match RPLiDAR A2M8 max range
+        'Grid/CellSize': '0.05', # Resolution of the occupancy grid.
+        'Grid/MaxGroundHeight': '0.0', #'0.05', #'0.2', # Maximum ground height (0=disabled).
+        # 'Grid/MaxObstacleHeight': '5.0', # Maximum obstacles height (0=disabled).     
+        # 'Grid/3D': 'true',
+        
+
+        ### Registration
+        'Reg/Strategy': '2', # registration: 0=Vis, 1=Icp, 2=VisIcp
+        
+        'Reg/Force3DoF': 'false', #'false',  # 3D mode (due to Lite3 erractic, non-planar movement)
+
+        ### Lidar
+        # 'Icp/VoxelSize': '0.05',  # Uniform sampling voxel size (0=disabled). Default: 0.05
+        # 'Icp/MaxCorrespondenceDistance': '0.1', # Max distance for point correspondences. Default: 0.1
+
+        # ### RGBD / Loop closure
+        # 'RGBD/NeighborLinkRefining': 'True', # When a new node is added to the graph, the transformation of its neighbor link to the previous node is refined using registration approach selected
+        # 'RGBD/ProximityBySpace': 'true', # Detection over locations (in Working Memory) near in space.
+        # 'RGBD/ProximityMaxGraphDepth': '0', # Maximum depth from the current/last loop closure location and the local loop closure hypotheses. Set 0 to ignore.
+        # 'RGBD/AngularUpdate': '0.05',#'0.05', # Minimum angular displacement (rad) to update the map.
+        # 'RGBD/LinearUpdate': '0.05',#'0.05', # Minimum linear displacement (m) to update the map.
+        # 'RGBD/OptimizeFromGraphEnd': 'false', # Optimize graph from the newest node. If false, the graph is optimized from the oldest node of the current graph
+
+        ### Optimizer
+        # 'Optimizer/GravitySigma': '0',  # disable IMU constraints (2D mode)
+
+        ### Memory management
+        # 'Rtabmap/DetectionRate': '1.0',
+    }
+
+    if localization:
+        parameters['Mem/IncrementalMemory'] = 'false'
+        parameters['Mem/InitWMWithAllNodes'] = 'true'
+    else:
+        arguments.append('-d')
+
+    remappings = [
+        ('scan', '/scan'),
+        ('odom', '/odom'),
+        ('grid_map', '/map'),
+        ('rgb/image', '/camera/color/image_raw'),
+        ('rgb/camera_info', '/camera/color/camera_info'),
+        ('depth/image', '/camera/depth/image_rect_raw'),
+    ]
+
+    return LaunchDescription([
+        DeclareLaunchArgument("headless", default_value="false"),
+        DeclareLaunchArgument("use_rtabmap_rviz", default_value="false"),
+        DeclareLaunchArgument('use_sim_time', default_value='false'),
+        DeclareLaunchArgument('localization', default_value='false',
+                              description='Set to true to run in localization mode (reuse existing map)'),
+        DeclareLaunchArgument('database_path', default_value='~/.ros/rtabmap.db',
+                              description='Path to RTAB-Map database file'),
+
+        # RTAB-Map SLAM node (2D lidar only, ICP odometry)
+        Node(
+            package='rtabmap_slam',
+            executable='rtabmap',
+            name='rtabmap',
+            output='screen',
+            parameters=[parameters],
+            remappings=remappings,
+            arguments=arguments
+        ),
+
+        Node(
+            package='rtabmap_viz',
+            executable='rtabmap_viz',
+            name='rtabmap_viz',
+            output='screen',
+            parameters=[parameters],
+            remappings=remappings,
+            arguments=arguments,
+            condition=IfCondition(
+                PythonExpression(["'", use_rtabmap_rviz, "' == 'true' and '", headless, "' != 'true'"])
+            ),
+        ),
+    ])
